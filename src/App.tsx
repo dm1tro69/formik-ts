1af234d961d139382a080880e60ddb8137d8f23c
dm1tro69 <dimolg22@gmail.com>
2021-11-27 21:39:13 +0200

    add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css'
+import {Formik} from "formik";
+
+interface FormModal {
+    name: string
+    username: string
+    email: string
+    dob: Date | undefined
+    password: string
+}
 
 function App() {
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik<FormModal>
+          initialValues={{
+              name: '',
+              username: '',
+              email: '',
+              dob: undefined,
+              password: ''
+          }}
+          onSubmit={(values) => {
+             alert(JSON.stringify(values))
+          }}
+      >
+          {({handleSubmit, values, handleChange, setFieldValue}) => {
+
+              const dateOfChange = (date: Date | null) => {
+              setFieldValue('dob', date)
+          }
+          return (
+              <form onSubmit={handleSubmit}>
+                  <label>Name</label>
+                  <input
+                      type="text"
+                      value={values.name}
+                      onChange={handleChange}
+                      name={'name'}
+                      placeholder={'Type your name here'}
+                  />
+                  <label>Username</label>
+                  <input
+                      value={values.username}
+                      onChange={handleChange}
+                      type="text"
+                      name={'username'}
+                      placeholder={'Type your username here'}
+                  />
+                  <label>Email</label>
+                  <input
+                      value={values.email}
+                      onChange={handleChange}
+                      type="email"
+                      name={'email'}
+                      placeholder={'Type your email here'}
+                  />
+                  <label>Date of Birth</label>
+                  <DatePicker
+                      value={values.dob?.toLocaleDateString()}
+                      selected={values.dob}
+                      name={'dob'}
+                      placeholderText={'This is where you set your Date of Birth'}
+                      onChange={dateOfChange}
+                  />
+                  <label>Password</label>
+                  <input
+                      value={values.password}
+                      onChange={handleChange}
+                      type="password"
+                      name={'password'}
+                      placeholder={'Type your password here'}
+                  />
+                  <button type={'submit'}>Add Your Info</button>
+              </form>
+              )
+
+
+
+          }}
+
+      </Formik>
     </div>
   );
 }
